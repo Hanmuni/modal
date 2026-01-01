741cb7d0f69008efb67e6abfd88df48b7da50961
Hong-Hanh Chu <67581292+Hanmuni@users.noreply.github.com>
2026-01-01 20:30:08 +0100

    add module window

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert('JS File Linked!')
+const clickBtn = document.querySelector(".click-btn");
+const closeBtn = document.querySelector(".close-btn");
+
+clickBtn.addEventListener("click", () => {
+  document.body.classList.add("modal-open");
+  console.log("Click me!");
+});
+
+closeBtn.addEventListener("click", () => {
+  document.body.classList.remove("modal-open");
+});
